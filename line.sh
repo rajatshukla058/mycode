9c52b484c44fe5022920d7cdc880464e6f5284a2
Rajat <rajatshukla058@gmail.com>
2017-10-24 01:15:54 -0700

    new

--- line.sh
+++ line.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-LINE_NUM=1
-while read LINE
-do 
-echo "${LINE_NUM} : ${LINE} "
-(( LINE_NUM++))
-done 
+#LINE_NUM=1
+#while read LINE
+#do 
+#echo "${LINE_NUM} : ${LINE} "
+#(( LINE_NUM++))
+#done 
 ###< /etc/fstab
+grep ext4 < /etc/fstab | while read LINE
+do 
+echo "ext4 : ${LINE}"
+done
