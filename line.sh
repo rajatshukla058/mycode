f93441b6fc6475410889089f097b1bfc34b8b360
Rajat <rajatshukla058@gmail.com>
2017-10-26 01:33:04 -0700

    new1

--- line.sh
+++ line.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
-#LINE_NUM=1
-#while read LINE
+#!/bin/bash -x
+LINE_NUM=1
+#while re 'blkid' to print the universally unique identifier for a
+# device; this may be used with UUID= as a more robust way to name devices
+# that works even if disks are added and removed. See fstab(5).
+#
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+# / was on /dev/sda1 during installation
+#UUID=9a92c215-92bb-4651-973d-2c9f3b714cdd /               ext4    errors=remount-ro 0       1
+# swap was on /dev/sda5 during installation
+#UUID=c2e1502e-ae2d-4094-bb44-8cdf313aec9d none            swap    sw              0       0
+#/dev/fd0        /media/floppy0  auto    rw,user,noauto,exec,utf8 0       0
+#ad LINE
 #do 
 #echo "${LINE_NUM} : ${LINE} "
 #(( LINE_NUM++))
 #done 
 ###< /etc/fstab
-grep ext4 < /etc/fstab | while read LINE
+grep ext4 /etc/fstab | while read  MP FS REST
 do 
-echo "ext4 : ${LINE}"
+echo "${LINE_NUM}: FILESYSTEM : ${MP}"
+echo "${LINE_NUM}: MOUNTPOINT : ${FS}"
+echo "${LINE_NUM}: REST       : ${REST}"
+(( LINE_NUM++))
 done
